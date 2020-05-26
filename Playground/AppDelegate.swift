4cf11adbded1b49a57bc762db1bd44f41ae5519d
zou-pp <zoupanpan@xindong.com>
2020-05-26 14:31:19 +0800

    Initial Commit

---
+++ Playground/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Playground
+//
+//  Created by zpp on 2020/5/26.
+//  Copyright © 2020 zpp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
